adf865ea9d024521c1bd3db1f15e3e994f743508
”Joonas <joonas.kinn@gmail.com>
2020-03-01 21:17:47 +0200

    add backend and Heroku

---
+++ muunna-backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
