96bfe81bfcbc9318b3f168530c251fe0f4973366
”Joonas <joonas.kinn@gmail.com>
2020-03-05 21:43:26 +0200

    fix sidebar ulrs

--- muunna-frontend/src/components/Sidebar.js
+++ muunna-frontend/src/components/Sidebar.js
@@ -6,16 +6,16 @@ const Sidebar = () => {
     return (
         <ListGroup>
             <ListGroup.Item>
-                <Link to='/matematiikka/desimaali-binaari-muunnin'>Desimaaliluku - binäärilukumuunnin</Link>
+                <Link to='/yksikkomuuntimet/desimaali-binaari-muunnin'>Desimaaliluku - binäärilukumuunnin</Link>
             </ListGroup.Item>
             <ListGroup.Item>
-                <Link to='/matematiikka/binaari-desimaali-muunnin'>Binääriluku - desimaalilukumuunnin</Link>
+                <Link to='/yksikkomuuntimet/binaari-desimaali-muunnin'>Binääriluku - desimaalilukumuunnin</Link>
             </ListGroup.Item>
             <ListGroup.Item>
-                <Link to='/matematiikka/desimaali-heksadesimaali-muunnin'>Desimaaliluku - heksadesimaalilukumuunnin</Link>
+                <Link to='/yksikkomuuntimet/desimaali-heksadesimaali-muunnin'>Desimaaliluku - heksadesimaalilukumuunnin</Link>
             </ListGroup.Item>
             <ListGroup.Item>
-                <Link to='/matematiikka/desimaali-oktaaliluku-muunnin'>Desimaaliluku - oktaalilukumuunnin</Link>
+                <Link to='/yksikkomuuntimet/desimaali-oktaaliluku-muunnin'>Desimaaliluku - oktaalilukumuunnin</Link>
             </ListGroup.Item>
         </ListGroup>
     )
