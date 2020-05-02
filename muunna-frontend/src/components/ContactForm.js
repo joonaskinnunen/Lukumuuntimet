618b592316c3fc6fc4bc55e966b156ec12bea9bc
”Joonas <joonas.kinn@gmail.com>
2020-05-02 21:00:37 +0300

    Finish the contact form

--- muunna-frontend/src/components/ContactForm.js
+++ muunna-frontend/src/components/ContactForm.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react'
-import { Breadcrumb, InputGroup, FormControl, Form, Button } from 'react-bootstrap'
+import { Breadcrumb, Form, Button } from 'react-bootstrap'
 import { LinkContainer } from "react-router-bootstrap"
 import axios from 'axios'
+import Notification from './Notification'
 
 const ContactForm = () => {
-    const [values, setValues] = useState({email: '', message: ''})
+    const [values, setValues] = useState({ name: '', email: '', message: '' })
+    const [notificationMessages, setNotificationMessages] = useState({ notificationMessage: '', notificationErrorMessage: ''})
     const [validated, setValidated] = useState(false)
 
     useEffect(() => {
         document.title = 'Ota yhteyttä - Laske & Muunna'
     })
+
+    const formDivStyle = {
+        margin: '20px'
+    }
 
     const handleEmailChange = (value) => {
         setValues({ ...values, email: value })
@@ -19,29 +25,38 @@ const ContactForm = () => {
         setValues({ ...values, message: value })
     }
 
+    const handleNameChange = (value) => {
+        setValues({ ...values, name: value })
+    }
+
     const handleSubmit = (event) => {
         const form = event.currentTarget
-        console.log(form)
         event.preventDefault();
         if (form.checkValidity() === false) {
             event.stopPropagation();
         } else {
             axios({
-                method: "POST", 
-                url:"http://localhost:3001/send", 
+                method: "POST",
+                url: "http://localhost:3001/send",
                 data: values
-              }).then((response)=>{
-                if (response.data.status === 'success'){
-                  console.log("Viesti lähetetty.") 
-                  this.resetForm()
-                } else if(response.data.status === 'fail'){
-                  console.log("Viestin lähettäminen epäonnistui.")
+            }).then((response) => {
+                if (response.data.status === 'success') {
+                    console.log("Viesti lähetetty.")
+                    setNotificationMessages({notificationErrorMessage: '', notificationMessage: 'Viestin lähetys onnistui. Kiitos viestistäsi!'})
+                    setValues({ name: '', email: '', message: '' })
+                    setTimeout(() => setNotificationMessages({notificationErrorMessage: '', notificationMessage: ''}), 5000)
+                    console.log(notificationMessages)
+                } else if (response.data.status === 'fail') {
+                    console.log(response.data)
+                    console.log("Viestin lähettäminen epäonnistui.")
+                    setNotificationMessages({notificationErrorMessage: 'Viestin lähettäminen epäonnistui.', notificationMessage: ''})
+                    setTimeout(() => setNotificationMessages({notificationErrorMessage: '', notificationMessage: ''}), 5000)
                 }
-              })
+            })
         }
 
         setValidated(true);
-    };
+    }
 
     return (
         <div>
@@ -61,29 +76,39 @@ const ContactForm = () => {
             <p>
                 Kehitysehdotuksia? Huomasitko virheen sivustolla? Voit ottaa yhteyttä sivuston ylläpitoon alla olevalla lomakkeella.
             </p>
-            <div>
+            <div style={formDivStyle}>
                 <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <Form.Group controlId="formBasicEmail">
+                    <Form.Group>
+                        <Form.Label>Nimi</Form.Label>
+                        <Form.Control placeholder="Syötä nimesi" required value={values.name} onChange={({ target }) => handleNameChange(target.value)}/>
+                        <Form.Control.Feedback>
+                            Hyvältä näyttää!
+                        </Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">
+                            Syötä nimesi
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
                         <Form.Label>Sähköpostiosoite</Form.Label>
-                        <Form.Control required type="email" placeholder="Syötä sähköpostiosoite" value={values.email} onChange={({ target }) => handleEmailChange(target.value)}/>
+                        <Form.Control required type="email" placeholder="Syötä sähköpostiosoite" value={values.email} onChange={({ target }) => handleEmailChange(target.value)} />
                         <Form.Text className="text-muted">
                             Emme koskaan jaa sähköpostiosoitettasi ulkopuolisille.
                         </Form.Text>
                         <Form.Control.Feedback>
-                        Hyvältä näyttää!
+                            Hyvältä näyttää!
                         </Form.Control.Feedback>
                         <Form.Control.Feedback type="invalid">
-                        Syötä sähköpostiosoite
+                            Syötä sähköpostiosoite
                         </Form.Control.Feedback>
                     </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Group>
                         <Form.Label>Viesti</Form.Label>
-                        <Form.Control required as="textarea" rows="3" placeholder="Kirjoita viestisi tähän" value={values.message} onChange={({ target }) => handleMessageChange(target.value)}/>
+                        <Form.Control required as="textarea" rows="3" placeholder="Kirjoita viestisi tähän" value={values.message} onChange={({ target }) => handleMessageChange(target.value)} />
                         <Form.Control.Feedback>
-                        Hyvältä näyttää!
+                            Hyvältä näyttää!
                         </Form.Control.Feedback>
                         <Form.Control.Feedback type="invalid">
-                        Syötä viesti
+                            Syötä viesti
                         </Form.Control.Feedback>
                     </Form.Group>
                     <Button variant="primary" type="submit">
@@ -91,6 +116,7 @@ const ContactForm = () => {
                     </Button>
                 </Form>
             </div>
+            <Notification message={notificationMessages.notificationMessage} errorMessage={notificationMessages.notificationErrorMessage}/>
         </div>
     )
 }
